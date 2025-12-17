b719974613767ef697a33a8e7854621b8ccb269a
P sayedbasha <patansayedbasha786@gmail.com>
2025-12-17 19:26:02 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,39 @@
+#logical operator on the two persons
+p1=[6.5,80]
+p2=[7.0,50]
+st1=p1[0]>p2[0]
+st2=p1[1]<p2[1]
+print(st1)
+print(st2)
+res=st1 and st2
+print(res)
+
+
+p1=[7.1,80]
+p2=[7.0,50]
+st1=p1[0]>p2[0]
+st2=p1[1]<p2[1]
+print(st1)
+print(st2)
+res=st1 and st2
+print(res)
+
+
+p1=[6.5,80]
+p2=[7.0,90]
+st1=p1[0]>p2[0]
+st2=p1[1]<p2[1]
+print(st1)
+print(st2)
+res=st1 and st2
+print(res)
+
+
+p1=[6.5,80]
+p2=[7.0,90]
+st1=p1[0]<p2[0]
+st2=p1[1]<p2[1]
+print(st1)
+print(st2)
+res=st1 and st2
+print(res)
